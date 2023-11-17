2a01e50aa28011094c1218a28480c05729be68b2
isaias oliveira <i.oliveiraa02@gmail.com>
2023-11-16 21:49:00 -0300

    Good solution

---
+++ src/model/exceptions/DomainException.java
@@ -0,0 +1,20 @@
+package model.exceptions;
+
+public class DomainException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	public DomainException (String msg) {
+		super(msg);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
